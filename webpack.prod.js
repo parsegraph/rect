69c026aadf6be4c6a0df94962e4e5eb2e4e79f27
Aaron Faanes <dafrito@gmail.com>
2021-12-09 01:04:00 -0600

    Use parsegraph_rect as global library

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ module.exports = {
     path: path.resolve(__dirname, "dist-prod"),
     filename: "parsegraph-rect.js",
     globalObject: "this",
-    library: "parsegraph",
+    library: "parsegraph_rect",
     libraryTarget: "umd",
   },
   module: {
